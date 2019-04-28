df1838e5f1d75a42ef3aac27faf2594483d11f7e
Harshil Patel <Hspatel2@terpmail.umd.edu>
2019-04-28 16:15:22 -0400

    proj 9 done

--- assignments/9_Crypto_I/writeup/crack.py
+++ assignments/9_Crypto_I/writeup/crack.py
@@ -1,19 +1,31 @@
-#!/usr/bin/env python3
-
 import hashlib
 import string
 
 def crack():
-    hashes = # open and read hashes.txt
-    passwords = # open and read passwords.txt
+
+    # Open and read hashes.txt
+    with open("hashes.txt", "r") as h:
+        hashes = h.read().splitlines()
+
+    # Open and read passwords.txt
+    with open("passwords.txt", "r") as p:
+        passwords = p.read().splitlines()
+
+    # ASCII Lowercase letters
     characters = string.ascii_lowercase
 
+    # Prepend lowercase letter to a word in passwords
     for c in characters:
         for p in passwords:
+            
             # crack hashes
+            val = c + p
+            hashkey = hashlib.sha256(val).hexdigest()
 
             # print hashes as 'input:hash'
             # i.e.  yeet:909104cdb5b06af2606ed4a197b07d09d5ef9a4aad97780c2fe48053bce2be52
+            if hashkey in hashes:
+                print val,":",hashkey
 
 if __name__ == "__main__":
     crack()
--- assignments/9_Crypto_I/writeup/server_crack.py
+++ assignments/9_Crypto_I/writeup/server_crack.py
@@ -6,17 +6,47 @@ import socket
 import time
 
 def server_crack():
-    hashes = # open and read hashes.txt
-    passwords = # open and read passwords.txt
+
+    # Open and read hashes.txt
+    with open("hashes.txt", "r") as h:
+        hashes = h.read().splitlines()
+
+    # Open and read passwords.txt
+    with open("passwords.txt", "r") as p:
+        passwords = p.read().splitlines()
+
+    # ASCII Lowercase letters
     characters = string.ascii_lowercase
-    server_ip = 'put_your_ip_here'
-    server_port = 00000
+
+    # Server information
+    server_ip = "134.209.128.58"
+    server_port = 1337
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((server_ip, server_port))
-    data = s.recv(1024)
-    # parse data
-    # crack 3 times
+    
+    # Parse data and crack 3 times
+    i = 0
+    while i < 3:
+        
+        data = s.recv(1024)
 
+        # Prepend lowercase letter to a word in passwords
+        for c in characters:
+            for p in passwords:
+            
+                # crack hashes
+                val = c + p
+                hashkey = hashlib.sha256(val).hexdigest()
+            
+                if hashkey in data:
+                    print val,":",hashkey
+                    s.send(val+"\n")
+        i = i + 1
+
+    # Obtain flag after 3 successful cracks
+    data = s.recv(1024)
+    print data
+                    
 if __name__ == "__main__":
     server_crack()
